8051b772ecc818cdb536f06bd265c1d66d8786eb
Roman Goduhin <superchewits2000@gmail.com>
2023-03-21 22:00:16 +0300

    fix: fix incorrect extracting tags from capitalized string

--- my-app/src/helpers/extractTags.ts
+++ my-app/src/helpers/extractTags.ts
@@ -1,6 +1,6 @@
 import {Tags} from "@globalTypes";
 
-const REGEX_PATTERN = /#[a-z0-9_]+/g;
+const REGEX_PATTERN = /#[a-zA-Z0-9_]+/g;
 
 export const extractTags = (text: string): Tags => {
   const tagsArr = text.match(REGEX_PATTERN);
