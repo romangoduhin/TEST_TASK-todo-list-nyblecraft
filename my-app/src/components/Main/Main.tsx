20899a9d3c50f8e06229b26c96d9af52ef929137
Roman Goduhin <superchewits2000@gmail.com>
2023-03-21 19:40:48 +0300

    feat: add empty content text

--- my-app/src/components/Main/Main.tsx
+++ my-app/src/components/Main/Main.tsx
@@ -3,12 +3,14 @@ import {MainProps} from "./Main.types";
 import {NoteList} from "./NoteList";
 import {TagsList} from "./TagsList";
 import styles from "./Main.module.scss";
+import {Text} from "@ui";
+import {isArrEmpty} from "@helpers";
 
 export const Main: React.FC<MainProps> = ({deleteNote, editNote, notes, tags}) => {
-  return (
-    <main className={styles.main}>
-      <TagsList tags={tags}/>
-      <NoteList notes={notes} deleteNote={deleteNote} editNote={editNote}/>
-    </main>
-  );
+  if (isArrEmpty(notes)) return <Text className={styles.emptyContent} size={"large"}>Nothing To Show</Text>
+
+  return <main className={styles.main}>
+    <TagsList tags={tags}/>
+    <NoteList notes={notes} deleteNote={deleteNote} editNote={editNote}/>
+  </main>
 };
